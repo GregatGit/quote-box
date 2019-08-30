1d52392473751bc55356b441d4bf7ea519f40062
gregatgit <gregdd@outlook.com>
2019-08-30 23:22:37 +1000

    Updates

---
+++ precache-manifest.f31930bd1b74a20378bb934358b337ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c1f69ca855076b53a7503da70bc10d",
+    "url": "/quote-box/index.html"
+  },
+  {
+    "revision": "ef8fac471c4c2d402c46",
+    "url": "/quote-box/static/css/main.d0d36c6b.chunk.css"
+  },
+  {
+    "revision": "29ebb5d8d2b219492ec9",
+    "url": "/quote-box/static/js/2.2e59ca9b.chunk.js"
+  },
+  {
+    "revision": "ef8fac471c4c2d402c46",
+    "url": "/quote-box/static/js/main.c423f674.chunk.js"
+  },
+  {
+    "revision": "38ee6958098b51425cd0",
+    "url": "/quote-box/static/js/runtime~main.2d633562.js"
+  }
+]);
